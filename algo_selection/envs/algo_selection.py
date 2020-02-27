8deaef5fd1b3859bc6a049f419ce5cf4f516f798
a1noack <a1noack@csbsju.edu>
2020-02-27 15:59:37 -0800

    make small changes to algo_selection env

--- algo_selection/envs/algo_selection.py
+++ algo_selection/envs/algo_selection.py
@@ -15,27 +15,39 @@ import gym
 import numpy as np
 import pandas as pd
 import statistics as st
-
+import math
+import time
 
 class AlgoSelection(gym.Env):
-    def __init__(self, data_path, budget, stdev_thresh):
-        self._load_datafile()
-        self.budget = budget
-        self.preds = []
+    def __init__(self):
+        self.timing = pd.read_pickle('env_specs/analytic_timings.pkl')
+        self.predictions = pd.read_pickle('env_specs/prediction_data.pkl')
+        self.analytic_names = list(self.timing['analytic'])
+        self.analytic_times = np.array(self.timing['avg_proc'])
+        
+        self.num_algos = len(self.analytic_names)
+        self.curr_state = np.zeros(self.num_algos)
+        self.budget = 1000 # time in seconds
         self.time = 0
-        self.index = 0
+        self.preds = []
+        self.index = 6
         self.episode_done = False
         self.curr_episode = 0
-        self.stdev_thresh = stdev_thresh
-        self.curr_state = np.zeros(self.num_algos)
+        self.stdev_thresh = .35
+        
+    def _algo_used(self, algo):
+        return self.curr_state[algo] == 1.
     
-    def _load_datafile(data_path):
-        data = pd.read_csv(data_path, sep='\t').values
-        self.X, self.Y = data[:, 0], data[:, 1]
-        self.algos = data[:, 2:]
-        self.num_algos = self.algos.shape[1]
+    def random_action(self):
+        unused = np.where(self.curr_state == 0.)[0]
+        num_unused = np.size(unused)
+        i = np.random.randint(num_unused)
+        return unused[i]
+    
+    def unused_algos(self):  
+        return not np.sum(self.curr_state) == np.size(self.curr_state)
 
-    def step(self, action):
+    def step(self, algo, verbose=False):
         """
         The agent takes a step in the environment.
 
@@ -65,14 +77,37 @@ class AlgoSelection(gym.Env):
                  However, official evaluations of your agent are not allowed to
                  use this for learning.
         """
-        self.current_state[action] = 1.
-        self.time += self.algos[self.index, action][1]
-        new_pred = self.algos[self.index, action][0]
-        preds.append(new_pred)
-        if st.stdev(preds) < thresh:
-            self.episode_done = True
+        if self._algo_used(algo):
+            return self.curr_state, 0., False, {}
         
-        return self.current_state, reward, self.episode_done, {}
+        self.curr_state[algo] = 1.
+        
+        if self.time + self.analytic_times[algo] >= self.budget:
+            return self.curr_state, 0., False, {}
+        
+        self.time += self.analytic_times[algo]      
+        algo_name = self.analytic_names[algo]
+        new_pred = float(self.predictions.at[self.index, algo_name + '_score'])
+        
+        reward = 0
+        if not math.isnan(new_pred):
+            self.preds.append(new_pred)
+            reward = 1 - (new_pred - float(self.predictions.at[self.index, 'label']))**2
+        
+        if len(self.preds) > 1:
+            stdev = st.stdev(self.preds)
+            if stdev < self.stdev_thresh:
+                self.episode_done = True
+   
+        if not self.unused_algos():
+            self.episode_done = True
+            
+        if verbose:
+            print('running algo #{}, {}'.format(algo, algo_name))
+            print('\ttime to run algo: {}s'.format(self.analytic_times[algo]))      
+            print("\talgo prediction: {:.3f}".format(new_pred))           
+
+        return self.curr_state, reward, self.episode_done, {}
 
     def reset(self):
         """
@@ -84,13 +119,15 @@ class AlgoSelection(gym.Env):
         """
         self.preds = []
         self.time = 0
-        self.index = self.index + 1
+        if self.index >= self.predictions.shape[0] - 1:
+            self.index = 0
+        else:
+            self.index = self.index + 1
         self.episode_done = False
         self.curr_episode += 1
         self.curr_state = np.zeros(self.num_algos)
         
-        return self.curr_state()
+        return self.curr_state
 
     def _render(self, mode='human', close=False):
         return
-
